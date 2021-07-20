283a526719cc31a1e32010f3def9216c7c655b9f
po468 <po468@protonmail.com>
2021-07-20 08:08:28 -0400

    added table

---
+++ conversion-table.sh
@@ -0,0 +1,37 @@
+otpath="conv-table.txt"
+
+echo "" > $otpath
+  echo "--------------------------------" >> $otpath
+  echo "" >> $otpath
+
+
+
+
+echo "Conversion Table No.1 (EN)">> $otpath
+
+echo "     B-70  P-80  FIG-90">> $otpath
+
+echo "A-1  C-71  Q-81  (.)-91">> $otpath
+
+echo "E-2  D-72  R-82  (:)-92">> $otpath
+
+echo "I-3  F-73  S-83  (')-93">> $otpath
+
+echo "N-4  G-74  U-84  ( )-94">> $otpath
+
+echo "O-5  H-75  V-85  (+)-95">> $otpath
+
+echo "T-6  J-76  W-86  (-)-96">> $otpath
+
+echo "     K-77  X-87  (=)-97">> $otpath
+
+echo "     L-78  Y-88  REQ-98">> $otpath
+
+echo "     M-79  Z-89  SPC-99">> $otpath
+
+echo "Code-0     -E   +D">> $otpath
+
+echo "" >> $otpath
+echo "" >> $otpath
+echo "" >> $otpath
+
